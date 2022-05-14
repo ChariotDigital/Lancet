481c56c0d6e8f56e4e99fbe7782226adf0463534
Dennis Towns <89202797+ChariotDigital@users.noreply.github.com>
2022-05-14 19:10:47 -0400

    working log out routing to home page

--- src/components/ProfileBadge/ProfileBadge.jsx
+++ src/components/ProfileBadge/ProfileBadge.jsx
@@ -3,11 +3,18 @@ import Link from "next/link";
 import { useMoralis } from "react-moralis";
 import {Blockie} from 'web3uikit'
 import { useWindowSize } from "../../hooks/useWindowSize";
+import { useRouter } from 'next/router';
 
 
 const ProfileBadge = (props) => {
     const {isAuthenticated, user, logout} = useMoralis();
     const size = useWindowSize();
+    const router = useRouter();
+
+    const logOutUser = () => {
+      logout()
+      router.push('/')
+    }
 
     return (
         <div className="header-btn-devider ml-auto ml-lg-5 pl-2 d-none d-xs-flex align-items-center">
@@ -29,26 +36,36 @@ const ProfileBadge = (props) => {
                         </Dropdown.Toggle>
                         {size.width <= 991 ? (
                           <Dropdown.Menu className="gr-menu-dropdown border-0 border-width-2 py-2 w-auto bg-default" key="1">
-                            <Link href="/#">
+                            <Link href="/dashboard-main">
+                              <a className="dropdown-item py-2 font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Dashboard</a>
+                            </Link>
+                            <Link href="/become-provider">
+                              <a className="dropdown-item py-2 font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Become a Provider</a>
+                            </Link>
+                            <Link href="/dashboard-settings">
                               <a className="dropdown-item py-2 font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Settings</a>
                             </Link>
-                            <Link href="/#">
+                            <Link href={user.get("isProvider") === true ? '/candidate-profile' : '/company-profile'}>
                               <a className="dropdown-item py-2 font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Edit Profile</a>
                             </Link>
-                            <Link href="/#">
-                              <a className=" dropdown-item py-2 text-red font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Log Out</a>
-                            </Link>
+                            <button onClick={() => logOutUser()} className=" dropdown-item py-2 text-red font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Log Out</button>
                           </Dropdown.Menu>
                         ) : (
                           <div className="dropdown-menu gr-menu-dropdown dropdown-right border-0 border-width-2 py-2 w-auto bg-default" key="2">
-                            <Link href="/#">
+                            <Link href="/dashboard-main">
+                              <a className="dropdown-item py-2 font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Dashboard</a>
+                            </Link>
+                            <Link href="/become-provider">
+                              <a className="dropdown-item py-2 font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Become a Provider</a>
+                            </Link>
+                            <Link href="/dashboard-settings">
                               <a className="dropdown-item py-2 font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Settings</a>
                             </Link>
-                            <Link href="/#">
+                            <Link href={user.get("isProvider") === true ? '/candidate-profile' : '/company-profile'}>
                               <a className="dropdown-item py-2 font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Edit Profile</a>
                             </Link>
-                            
-                              <button onClick={() => logout()} className=" dropdown-item py-2 text-red font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Log Out</button>
+                    
+                              <button onClick={() => logOutUser()} className=" dropdown-item py-2 text-red font-size-3 font-weight-semibold line-height-1p2 text-uppercase">Log Out</button>
                             
                           </div>
                         )}
